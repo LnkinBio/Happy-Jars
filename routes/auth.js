f3a3c22504e81c964c609069c8185ea7b56ede91
Viswanath Keerthi <viswanath608@gmail.com>
2019-01-07 01:11:54 +0530

    Auth part done, please use /auth/login and /auth/register to try

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var router = express.Router();
 
 /* Models */
@@ -129,6 +130,52 @@ router.get('/logout', function(req, res){
 	req.logout();
 	res.redirect("/auth/login")
 });
+
+
+/* Facebook Auth */
+
+passport.use(new FacebookStrategy({
+    clientID: "#",
+    clientSecret: "#",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile)
+    User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+      if (err) return done(err);
+      if (user) return done(null, user);
+      else {
+        // if there is no user found with that facebook id, create them
+        var newUser = new User();
+
+        // set all of the facebook information in our user model
+        newUser.facebook.id = profile.id;
+        newUser.facebook.token = accessToken;
+        newUser.facebook.name  = profile.displayName;
+        if (typeof profile.emails != 'undefined' && profile.emails.length > 0)
+          newUser.facebook.email = profile.emails[0].value;
+
+        // save our user to the database
+        newUser.save(function(err) {
+            if (err) throw err;
+            return done(null, newUser);
+        });
+      }
+    });
+  }
+));
+
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook',{
+	failureRedirect: '/auth/login'
+}),
+  function(req, res) {
+    // Successful authentication
+    console.log(req.user)
+    res.redirect('/');
+  }
+);
 
 
 
